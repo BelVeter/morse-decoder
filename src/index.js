4740e9722c19900c3ee1e787c1b01eb8aa2ba4ad
belveter <dmitry.nayd@gmail.com>
2022-02-09 21:55:10 +0100

    feat: task 3 is done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,26 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let rez = [];
+    for(let i = 0; i < expr.length; i = i + 10) {
+        let morse = '';
+        for(let j = i; j < (i+10); j = j + 2){
+            //console.log(`${expr[j]}-${expr[j+1]}`)
+            if(expr[j]==0) continue
+            if(expr[j]=='*') {
+                morse = '**';
+                break;
+            }
+            let morseDidgit = expr[j].toString() + expr[j+1].toString();
+            if(morseDidgit == '10') morseLetter = '.';
+            if(morseDidgit == '11') morseLetter = '-';
+            
+            morse += morseLetter;
+        }
+        if (morse == '**') rez.push(' ');
+        else rez.push(MORSE_TABLE[morse]);
+    }
+    return rez.join('');
 }
 
 module.exports = {
